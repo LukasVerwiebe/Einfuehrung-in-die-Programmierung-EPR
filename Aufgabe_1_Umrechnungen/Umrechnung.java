9505de50b237f5fa075fea38885c48a9d8bbd3c3
Lukas Verwiebe <lukas.verwiebe.de>
2021-05-17 23:04:21 +0200

    Update Umrechnung.java

--- Aufgabe_1_Umrechnungen/Umrechnung.java
+++ Aufgabe_1_Umrechnungen/Umrechnung.java
@@ -1,75 +1,43 @@
-package umrechnungen;
-
 /**
+ * Aufgabe Nr. 1: Umrechnungen
+ * In dieser Aufgabe geht es darum, Ausdrücke zu bilden, die bestimmte 
+ * Berechnungen durchführen.
  * @author Lukas
  */
-public class Umrechnung {
-
+public class Umrechnungen {
+    
     /**
-     * @param args the command line arguments
+     * Diese Methode wird dafür verwendet um die Ausdrücke auszugeben.
+     * @param args wird nicht verwendet
      */
     public static void main(String[] args) {
         
-        /*
-            Aufgabe 1: Umrechnung von Temperaturen
-        */
-        int fahrenheit;
-        int celsius;
-        
-        fahrenheit = 40;
-        int temperatur = (fahrenheit - 32) * 5 / 9;
-        System.out.println("Fahreheit: " + fahrenheit + " Celsius: " + temperatur);
-        
-        fahrenheit = 41;
-        int temperatur2 = (fahrenheit - 32) * 5 / 9;
-        System.out.println("Fahreheit: " + fahrenheit + " Celsius: " + temperatur2);
+        /**
+         * Aufgabe 1: Temperatur umrechnen
+         * Umrechnung der Temperatur von Grad Fahrenheit in Grad Celsius.
+         */
+        System.out.println("Celsius 1: " + (int) ((5.0 / 9.0) * (40.0 - 32.0)));
+        System.out.println("Celsius 2: " + (int) ((5.0 / 9.0) * (41.0 - 32.0)));
         System.out.println("");
         
-        /*
-            Aufgabe 2: Geld Umrechnen
-        */
-        int schweizerFranken;
-        
-        schweizerFranken = 100;
-        double umrechnung = schweizerFranken / 1.1401;
-        System.out.println("Franken: " + schweizerFranken + " Euro: " + (int) umrechnung);
-                
-        schweizerFranken = 10000;
-        double umrechnung2 = schweizerFranken / 1.1401;
-        System.out.println("Franken: " + schweizerFranken + " Euro: " + (int) umrechnung2);
+        /**
+         * Aufgabe 2: Wechselkurs Umrechnung
+         * Umrechnung von Schweizerfranken in Euro.
+         */
+        System.out.println("EUR 1: " + (int) (100 / 1.1401));
+        System.out.println("EUR 1: " + (int) (10000 / 1.1401));
         System.out.println("");
         
-        /*
-            Aufgabe 3: Umrechnung Zeit
-        */
-        int minuten;
-        int stunden;
-        int restMinuten;
+        /**
+         * Aufgabe 3: Zeit Umrechnen
+         * Umrechnung von Minuten in volle Stunden.
+         */
+        System.out.println("Zeit 1: " + ((5 / 60) * 100 + (5 % 60)));
+        System.out.println("Zeit 2: " + ((59 / 60) * 100 + (59 % 60)));
+        System.out.println("Zeit 3: " + ((60 / 60) * 100 + (60 % 60)));
+        System.out.println("Zeit 4: " + ((61 / 60) * 100 + (61 % 60)));
+        System.out.println("Zeit 5: " + ((825 / 60) * 100 + (825 % 60)));
         
-        minuten = 5;
-        stunden = minuten / 60;
-        restMinuten = minuten % 60;        
-        System.out.println(stunden + ":" + restMinuten);
-        
-        minuten = 59;
-        stunden = minuten / 60;
-        restMinuten = minuten % 60;        
-        System.out.println(stunden + ":" + restMinuten);
-        
-        minuten = 60;
-        stunden = minuten / 60;
-        restMinuten = minuten % 60;        
-        System.out.println(stunden + ":" + restMinuten);
-        
-        minuten = 61;
-        stunden = minuten / 60;
-        restMinuten = minuten % 60;        
-        System.out.println(stunden + ":" + restMinuten);
-        
-        minuten = 825;
-        stunden = minuten / 60;
-        restMinuten = minuten % 60;        
-        System.out.println(stunden + ":" + restMinuten);  
     }
     
 }
