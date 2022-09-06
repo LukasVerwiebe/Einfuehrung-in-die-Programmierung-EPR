dc2ab3cbe0bdd0caa723af987f2913f964cb90df
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-09-06 02:49:46 +0200

    Update Wurzelberechnung.java

--- K_Aufgabe_11_Wurzelberechnung/Wurzelberechnung.java
+++ K_Aufgabe_11_Wurzelberechnung/Wurzelberechnung.java
@@ -1,23 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktika1;
-
 /**
- *
+ * Aufgabe Nr. 11: Wurzelberechnung
+ * Diese Klasse berechnet die Quadratwurzel einer Zahl
  * @author Lukas
  */
 public class Wurzelberechnung {
-    
-    public static void main(String[] args) {
-        
+    // Eingabe und Ausgabe von Testwerten
+    public static void main(String[] args) {        
         System.out.println("Soll: 9.5367431640625E-7 Ist: " + wurzel(0, 0.000001));
         System.out.println("Soll: 1.5 Ist: " + wurzel(2, 1));
         System.out.println("Soll: 3.4 Ist: " + wurzel(9, 2));
-        System.out.println("Soll: 3.023529411764706 Ist: " + wurzel(9, 0.4));
-        
+        System.out.println("Soll: 3.023529411764706 Ist: " + wurzel(9, 0.4));        
     }
     
      public static double wurzel(double x, double epsilon) { 
