bac16bbe62385a3f1430089c5d17cbbda3777e7f
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-09-06 03:55:52 +0200

    Update AbsteigendeZahl.java

--- R_Aufgabe_18_AbsteigendeZahl/AbsteigendeZahl.java
+++ R_Aufgabe_18_AbsteigendeZahl/AbsteigendeZahl.java
@@ -1,37 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktika1;
-
 /**
- *
+ * Aufgabe Nr. 18: Absteigende Zahl
+ * Klasse 
  * @author Lukas
  */
 public class AbsteigendeZahl {
-    
+    // Eingabe und Ausgabe von Testwerten
     public static void main(String[] args) {
         System.out.println(istAbsteigend(54321));
         System.out.println(istAbsteigend(123456));
     }
-    
+    // Methode zur Prüfung ob eine Zahl Absteigend ist oder nicht
     public static boolean istAbsteigend(int zahl) {
         boolean ergebnis = false;
         int zwischenWert1;
         int zwischenWert2;
-        
+        // Pürfung der Zhalenfolge
         while(zahl > 0) {
             zwischenWert1 = zahl % 10;
             zahl = zahl / 10;
             zwischenWert2 = zahl % 10;
-            
+            // Rückgabe ob Absteigend oder nicht
             if(zwischenWert1 <= zwischenWert2) {
                 ergebnis = true;
                 zahl = 0;
             } else {
-                ergebnis = false;
-                
+                ergebnis = false;                
             }
         }
         
