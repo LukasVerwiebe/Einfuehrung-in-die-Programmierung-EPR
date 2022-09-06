f7babe4f48a6b8d01cdc291ddab15252378f726d
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-09-06 03:41:26 +0200

    Update Quersumme.java

--- V_Aufgabe_22_Quersumme/Quersumme.java
+++ V_Aufgabe_22_Quersumme/Quersumme.java
@@ -1,28 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktika1;
-
 /**
- *
+ * Aufgabe Nr. 22: Quersumme
+ * Klasse zur Berechnung der Quersumme einer Zahl
  * @author Lukas
  */
 public class Quersumme {
-    
+    // Eingabe und Ausgabe von Testwerten
     public static void main(String[] args) {
         System.out.println(quersumme(123));
         System.out.println(quersumme(1234));
     }
-    
+    // Methode zur Berechnung der Quersumme
     public static int quersumme(int zahl) {
+        // Speicher für die Quersumme
         int ergebnis = 0;
+        // Speicher für den Zwischenwert der Berechnung
         int zwischenwert;
         
         while(zahl > 0) {
+            // Herauslösen einer Ziffern
             zwischenwert = zahl % 10;
-            zahl = zahl / 10;        
+            // Verkleinern der Zahl um eine Ziffer
+            zahl = zahl / 10;
+            // Der Zwischenwert wird addiert
             ergebnis = ergebnis + zwischenwert;
         }        
         return ergebnis;
