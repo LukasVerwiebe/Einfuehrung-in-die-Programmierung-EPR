ef43a7eee953dda463fb6fbd000e178255970fd5
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-09-06 03:28:54 +0200

    Update ZahlUmdrehen.java

--- N_Aufgabe_14_ZahlUmdrehen/ZahlUmdrehen.java
+++ N_Aufgabe_14_ZahlUmdrehen/ZahlUmdrehen.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktika1;
-
 /**
- *
+ * Aufgabe Nr. 14: Zahl Umdrehen
+ * Diese Klasse dreht eine Zahl um un gibt die neue Zahl als Integer Wert aus
  * @author Lukas
  */
 public class ZahlUmdrehen {
-    
+    // Eingabe und Ausgabe von Testwerten
     public static void main(String[] args) {
         System.out.println(dreheUm(0));
         System.out.println(dreheUm(1));
@@ -18,11 +12,12 @@ public class ZahlUmdrehen {
         System.out.println(dreheUm(155));
         System.out.println(dreheUm(123456789));
     }
-    
+    // Methode um eine Zahl zum umdrehen anzugeben und diese mit einem Leerwert an eine 
+    // weitere Methode weiterzugeben. Die Leerzahl wird mit dem umgedrehten Wert gefüllt.
     public static long dreheUm(long umzudrehendeZahl) { 
         return dreheUm(umzudrehendeZahl, 0); 
     }
-    
+    // Methode mit der die Zahl umgedreht wird. Die neue Zahl wird zurückgegeben.
     private static long dreheUm(long zahl, long umgedrehteZahl) {  
         return 0 == zahl 
             ? umgedrehteZahl 
