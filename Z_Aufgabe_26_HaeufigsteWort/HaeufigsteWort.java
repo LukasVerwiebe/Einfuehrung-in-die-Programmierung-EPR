a18944d99a69478f555ec0ad9d2a85d873ed0793
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2022-09-06 02:25:30 +0200

    Update HaeufigsteWort.java

--- Z_Aufgabe_26_HaeufigsteWort/HaeufigsteWort.java
+++ Z_Aufgabe_26_HaeufigsteWort/HaeufigsteWort.java
@@ -1,30 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktika1;
-
 /**
- *
+ * Aufgabe Nr. 26: HäufigesWort
+ * Diese Klasse gibt das Wort aus welches sich am Häufigsten in einem Array befindet
  * @author Lukas
  */
 public class HaeufigsteWort {
-    
+    // Eingabe und Ausgabe von Testwerten
     public static void main(String[] args) {
         String[] test = {"Haus", "Baum", "Auto", "Baum", "Auto", "Auto", "Haus", "Test", "Baum", "Auto"};
         String[] test2 = {};
-        System.out.println(gibHaeufigsteWort(test));
-        
-
-        
+        System.out.println(gibHaeufigsteWort(test));        
     }
-    
+    /**
+    * Diese Methode Zählt die Wörter dabei wird das Aktuell Häufigste Wort
+    * Zwischengespeichert und jeweils überschrieben sobald ein Wort 
+    * häufiger Vorkommt.
+    **/
     public static String gibHaeufigsteWort(String[] feld) {
+        // Speicherung des Wortes
         String wort = null;
+        // Zwischenspeicher für den Zähler
         int zählerAlt = 0;
         
         String temp = "";
+        // Mittels der Schleife wird jedes Wort einzeln gezählt 
+        // der Zähler und das Wort werden jeweils ersätzt sobald
+        // ein Wort öffters gezählt wurde.
         for(int i = 1; i < feld.length; i++){
             int zähler = 0;
             for(int j = feld.length; j >= i; j--) {
@@ -35,6 +35,7 @@ public class HaeufigsteWort {
                     
                 }
             }
+            
             if(zählerAlt == 0) {
                 zählerAlt = zähler;
                 wort = temp;
